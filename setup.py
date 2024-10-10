b6c47913140b013b788040b896f8081bbe0b5f84
Zhongkai Ren <zhongkai.ren@unity3d.com>
2024-10-10 18:47:44 +0300

    Re-arrange folder structure

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+# Read the contents of your README file for the long description
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+# Read requirements from requirements.txt
+with open("requirements.txt", "r") as fh:
+    requirements = fh.read().splitlines()
+
+setup(
+    name="airtable_sync",
+    version="0.1.1",
+    author="Zhongkai Ren",
+    author_email="zhongkai.ren@unity3d.com",
+    description="A Python package to synchronize GitHub issues with Airtable.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zhongkairen/airtable-sync",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+    install_requires=requirements,
+    include_package_data=True,  # Include non-Python files specified in MANIFEST.in
+)
