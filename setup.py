a91bb1c71a1906e9f98bd3aeb38eed5d218c186f
Zhongkai Ren <zhongkai.ren@unity3d.com>
2024-10-11 09:27:20 +0300

    Add command; exclude example config.json; rm dotenv

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 # Read the contents of your README file for the long description
 with open("README.md", "r") as fh:
     long_description = fh.read()
-    
+
 # Read requirements from requirements.txt
 with open("requirements.txt", "r") as fh:
     requirements = fh.read().splitlines()
 
 setup(
     name="airtable_sync",
-    version="0.1.1",
+    version="0.1.6",
     author="Zhongkai Ren",
     author_email="zhongkai.ren@unity3d.com",
     description="A Python package to synchronize GitHub issues with Airtable.",
@@ -24,7 +24,15 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.10',
+    python_requires='>=3.6',
     install_requires=requirements,
-    include_package_data=True,  # Include non-Python files specified in MANIFEST.in
+    include_package_data=True,
+    exclude_package_data={
+        'airtable_sync': ['config.json'],
+    },
+    entry_points={
+        'console_scripts': [
+            'airtable-sync=airtable_sync.main:main',
+        ],
+    },
 )
